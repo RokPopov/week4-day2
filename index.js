0dff73a6c48f59d775b463a4ab794647d45418e8
rokpopov <rokpopov90@gmail.com>
2020-08-04 15:40:06 +0200

    Initialising a server

--- index.js
+++ index.js
@@ -1,31 +1,45 @@
+// const express = require("express");
+// const user = require("./models").user;
+// const todoList = require("./models").todoList;
+// const todoItem = require("./models").todoItem;
+// const app = express();
+// const PORT = 4000;
+
+// app.get("/users", async (req, res) => {
+//   const users = await user.findAll();
+//   res.send(users);
+// });
+
+// app.get("/todolists", async (req, res) => {
+//   try {
+//     const todolists = await todoList.findAll();
+//     res.send(todolists);
+//   } catch (error) {
+//     console.log(error.message);
+//   }
+// });
+
+// app.get("/todoitems", async (req, res) => {
+//   try {
+//     const todoItems = await todoItem.findAll();
+//     res.send(todoItems);
+//   } catch (error) {
+//     console.log(error.message);
+//   }
+// });
+
+// app.listen(PORT, () => console.log(`Server started in port: ${PORT}`));
+
 const express = require("express");
-const user = require("./models").user;
-const todoList = require("./models").todoList;
-const todoItem = require("./models").todoItem;
 const app = express();
 const PORT = 4000;
 
-app.get("/users", async (req, res) => {
-  const users = await user.findAll();
-  res.send(users);
+const User = require("./models").user;
+
+app.use(express.json());
+
+app.post("/echo", (req, res) => {
+  res.json(req.body);
 });
 
-app.get("/todolists", async (req, res) => {
-  try {
-    const todolists = await todoList.findAll();
-    res.send(todolists);
-  } catch (error) {
-    console.log(error.message);
-  }
-});
-
-app.get("/todoitems", async (req, res) => {
-  try {
-    const todoItems = await todoItem.findAll();
-    res.send(todoItems);
-  } catch (error) {
-    console.log(error.message);
-  }
-});
-
-app.listen(PORT, () => console.log(`Server started in port: ${PORT}`));
+app.listen(PORT, () => console.log(`Server listens in port: ${PORT}`));
