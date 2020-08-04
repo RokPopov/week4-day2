60f59ada3c72bc0a7acefc48a7a13b736261fd86
rokpopov <rokpopov90@gmail.com>
2020-08-04 16:02:54 +0200

    Registering a post endpoint

--- index.js
+++ index.js
@@ -42,4 +42,18 @@ app.post("/echo", (req, res) => {
   res.json(req.body);
 });
 
+app.post("/users", async (req, res, next) => {
+  try {
+    const email = req.body.email;
+    if (!email || email === " ") {
+      res.status(400).send("Must provide an email address");
+    } else {
+      const user = await User.create(req.body);
+      res.json(user);
+    }
+  } catch (e) {
+    next(e);
+  }
+});
+
 app.listen(PORT, () => console.log(`Server listens in port: ${PORT}`));
